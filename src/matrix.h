5a04ece934af8046d15f3dac537a79f895ec5247
Veera Ragav <veeraragav0@gmail.com>
2020-02-18 21:05:05 -0500

    initial working commit

--- src/matrix.h
+++ src/matrix.h
@@ -3,56 +3,56 @@
 #include <stdexcept>
 #include <iostream>
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 namespace linalg{
 
 template<typename T>
 class matrix {
     private:
-        boost::shared_ptr<T[]> data{new T};
+        boost::shared_ptr<T[]> data;
         size_t rows;
         size_t cols;
 
     public:
-         matrix(){
-             rows = 0;
-             cols = 0;
-         }
+ 
+        matrix(size_t n_rows, size_t n_cols);
+        matrix(size_t n_rows, size_t n_cols, T* input_data);
 
-        matrix(size_t n_rows, size_t n_cols){
-            rows = n_rows;
-            cols = n_cols;
-        }
-
-        matrix(size_t n_rows, size_t n_cols, T* input_data){
-            rows = n_rows;
-            cols = n_cols;
-            for(size_t i = 0; i < rows*cols; i++){
-                this->data[i] = input_data[i];
-            } 
-        }
-
-        matrix<T>& operator = (const matrix<T>& m);
-        
         T& at(size_t i, size_t j);
 
         matrix<T> transpose();
 
         matrix<T> operator * (const T scalar);
         matrix<T> operator * (matrix<T> mat);
+        matrix<T>& operator = (const matrix<T>& m);
 
-        
         template<T> friend matrix<T> operator* (  T scalar,  const matrix<T>& mat) ;
-
-        void print();
 
         void check_range(size_t i, size_t j);
 
         size_t no_of_rows();
         size_t no_of_cols();
-
+        void print();
 };
 
+template <typename T> 
+matrix<T>::matrix(size_t n_rows, size_t n_cols){
+    rows = n_rows;
+    cols = n_cols;
+    size_t n =  rows * cols;
+    data = boost::make_shared<T[]>(n);
+}
 
+template <typename T> 
+matrix<T>::matrix(size_t n_rows, size_t n_cols, T* input_data){
+    rows = n_rows;
+    cols = n_cols;
+    size_t n =  rows * cols;
+    data = boost::make_shared<T[]>(n);
+    for(size_t i = 0; i < rows*cols; i++){
+        this->data[i] = input_data[i];
+    } 
+}
 
 template <typename T> 
 size_t matrix<T>::no_of_rows(){
@@ -63,6 +63,31 @@ size_t matrix<T>::no_of_rows(){
 template <typename T> 
 size_t matrix<T>::no_of_cols(){
     return this->cols;
+}
+
+template <typename T>
+matrix<T> matrix<T>::operator*(matrix<T> mat){
+
+    if(this->cols != mat.no_of_cols()){
+        throw std::domain_error("matrix dimension do not match");
+    }
+
+    matrix<T> res(this->rows, mat.no_of_cols());
+    
+    for (int i = 0; i < this->rows; i++)  
+    { 
+        for (int j = 0; j < mat.no_of_cols(); j++)  
+        { 
+            
+            for (int k = 0; k < this->cols; k++)  
+            { 
+              res.at(i, j)  = res.at(i, j) + this->at(i, k) * mat.at(k, j);
+
+            } 
+        } 
+    } 
+
+    return res;
 }
 
 template <typename T>
@@ -77,10 +102,7 @@ matrix<T> matrix<T>::operator*(const T scalar){
 
 template <typename T>
 matrix<T> operator*( T scalar,  matrix<T> mat){
-    //matrix<T> result_matrix(mat.no_of_rows(), mat.no_of_cols());
-//     for(size_t i = 0; i < mat.rows*mat.cols; i++){
-//        result_matrix.data[i] = mat.data[i] * scalar;
-//    }
+
    return mat.operator*(scalar);
 }
 
@@ -94,12 +116,15 @@ template <typename T>
 matrix<T>& matrix<T>::operator = (const matrix<T>& m){  
     //TODO: std::copy?? check self assignemnt and skip
     //std::cout<<"==============\n"<<std::endl;
+    if(m.cols != this->cols || m.rows != this->rows){
+        throw std::domain_error("matrices are not of same order");
+    }
     std::cout<<"= called"<<std::endl;
-   this->rows = m.rows;
-   this->cols = m.cols; 
-   for(size_t i = 0; i < rows*cols; i++){
-       this->data[i] = m.data[i];
-   } 
+    this->rows = m.rows;
+    this->cols = m.cols; 
+    for(size_t i = 0; i < rows*cols; i++){
+        this->data[i] = m.data[i];
+    } 
   
    return *this; 
 }
